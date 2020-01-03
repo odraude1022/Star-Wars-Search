a6e534adf561314acfb95002f7b46e3d0b259ad7
Eduardo Iglesias <eriglesias@ufl.edu>
2020-01-02 22:07:29 -0500

    add background image

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    // <ImageBackground source={require('./background.png')} style={{height: '100%', width: '100%'}}>
+    <ImageBackground source={require('./background.png')} style={{width: '100%', height: '100%'}}>
+      <View style={styles.container}>
+        <Text style={{color: 'white'}}>Open up App.js to start working on your app!</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 70,
+    // backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
