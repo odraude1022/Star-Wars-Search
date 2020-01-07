d50f479582c11201d71df2b86db80949a4cb0ce3
Eduardo Iglesias <eriglesias@ufl.edu>
2020-01-06 21:55:50 -0500

    allow moving between screens

--- DetailsScreen.js
+++ DetailsScreen.js
@@ -3,14 +3,20 @@ import { StyleSheet, Text, View, ImageBackground, Image, Button } from 'react-na
 
 class DetailsScreen extends React.Component {
   render() {
+    const {navigation} = this.props
     return (
       <ImageBackground source={require('./background.png')} style={{width: '100%', height: '100%'}}>
         <View style={styles.container}>
           <Image source={require('./logo.png')} style={{height: 100, width: 200}}/>
           <Text style={{color: 'white'}}>Details</Text>
+          <Text style={{color: 'white'}}>id: {navigation.getParam('id')}</Text>
+          <Text style={{color: 'white'}}>somethingElse: {navigation.getParam('somethingElse')}</Text>
           <Button
             title="Go to Home"
-            onPress={() => this.props.navigation.push('Home')}  
+            onPress={() => navigation.navigate('Home', {
+              id: 54,
+              somethingElse: "goodbye"
+            })}  
           />
         </View>
       </ImageBackground>
--- HomeScreen.js
+++ HomeScreen.js
@@ -3,14 +3,20 @@ import { StyleSheet, Text, View, ImageBackground, Image, Button } from 'react-na
 
 class HomeScreen extends React.Component {
   render() {
+    const {navigation} = this.props
     return (
       <ImageBackground source={require('./background.png')} style={{width: '100%', height: '100%'}}>
         <View style={styles.container}>
           <Image source={require('./logo.png')} style={{height: 100, width: 200}}/>
           <Text style={{color: 'white'}}>Home</Text>
+          <Text style={{color: 'white'}}>id: {navigation.getParam('id')}</Text>
+          <Text style={{color: 'white'}}>somethingElse: {navigation.getParam('somethingElse')}</Text>
           <Button
             title="Go to Details"
-            onPress={() => this.props.navigation.push('Details')}  
+            onPress={() => navigation.navigate('Details', {
+              id: 45,
+              somethingElse: "hello"
+            })}  
           />
         </View>
       </ImageBackground>
